60b45412ee4e682d99fb9efc5fd0d1d11f81a26e
alegalus <alejandrogalus83@yahoo.com.ar>
2022-07-03 17:24:42 -0300

    adaptacion a mobile

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import s from "./Home.module.css";
 import Lottie from "lottie-react";
@@ -8,15 +8,13 @@ function Home() {
   const [about, setAbout] = useState(false);
   const [proyects, setProyects] = useState(false);
   const [contact, setContact] = useState(false);
+  const [screen, setScreen] = useState();
+  const [show, setShow] = useState(false);
 
-  // const defaultOptions = {
-  //   loop: true,
-  //   autoplay: true,
-  //   animationData: codeando,
-  //   rendererSettings: {
-  //     preserveAspectRatio: "xMidYMid slice"
-  //   }
-  // };
+  useEffect(() => {
+    setScreen(window.innerWidth);
+  }, [screen]);
+
   let handleAboutIn = () => {
     setAbout(!about);
   };
@@ -41,28 +39,89 @@ function Home() {
         <Lottie animationData={codeando} loop={true} height={400} width={400} />
       </div>
       <div>
-        <div id={s.white}>
-          {/* <h1 className={s.titulo}>Hola.</h1> */}
-          {!about && <h1 onMouseEnter={handleAboutIn}>Hola.</h1>}
-          <Link to={"/about"} className={s.decoration}>
-            {about && <h1 className={s.segundoTitWhite} onMouseLeave={handleAboutOut}>Sobre Mi</h1>}
-          </Link>
-        </div>
+        {screen > 1200 && (
+          <>
+            <div id={s.white}>
+              {!about ? (
+                <h1 onMouseEnter={handleAboutIn}>Hola.</h1>
+              ) : (
+                <Link to={"/about"} className={s.decoration}>
+                  <h1
+                    className={s.segundoTitWhite}
+                    onMouseLeave={handleAboutOut}
+                  >
+                    Sobre Mi
+                  </h1>
+                </Link>
+              )}
+            </div>
 
-        <div className={s.text}>
-          {!proyects && <h1 onMouseEnter={handleProyIn}>Soy</h1>}
-          <Link to={"/Proyects"} className={s.decoration}>
-            {proyects && <h1 className={s.segundoTit} onMouseLeave={handleProyOut}>Proyectos</h1>}
-          </Link>
-        </div>
+            <div className={s.text}>
+              {!proyects ? (
+                <h1 onMouseEnter={handleProyIn}>Soy</h1>
+              ) : (
+                <Link to={"/Proyects"} className={s.decoration}>
+                  <h1 className={s.segundoTit} onMouseLeave={handleProyOut}>
+                    Proyectos
+                  </h1>
+                </Link>
+              )}
+            </div>
 
-        <div className={s.text}>
-          {!contact && <h1 onMouseEnter={handleConIn}>Alejandro</h1>}
-          <Link to={"/Contact"} className={s.decoration}>
-            {contact && <h1 className={s.segundoTit} onMouseLeave={handleConOut}>Contacto</h1>}
-          </Link>
-        </div>
+            <div className={s.text}>
+              {!contact ? (
+                <h1 onMouseEnter={handleConIn}>Alejandro</h1>
+              ) : (
+                <Link to={"/Contact"} className={s.decoration}>
+                  <h1 className={s.segundoTit} onMouseLeave={handleConOut}>
+                    Contacto
+                  </h1>
+                </Link>
+              )}
+            </div>
+          </>
+        )}
       </div>
+      <>
+        {screen < 1200 && (
+          <>
+            <div id={s.white}>
+              {!show ? (
+                <h1>Hola.</h1>
+              ) : (
+                <Link to={"/about"} className={s.decoration}>
+                  <h1 className={s.segundoTitWhite}>Sobre Mi</h1>
+                </Link>
+              )}
+            </div>
+
+            <div className={s.text}>
+              {!show ? (
+                <h1>Soy</h1>
+              ) : (
+                <Link to={"/Proyects"} className={s.decoration}>
+                  <h1 className={s.segundoTit}>Proyectos</h1>
+                </Link>
+              )}
+            </div>
+
+            <div className={s.text}>
+              {!show ? (
+                <h1>Alejandro</h1>
+              ) : (
+                <Link to={"/Contact"} className={s.decoration}>
+                  <h1 className={s.segundoTit}>Contacto</h1>
+                </Link>
+              )}
+            </div>
+          </>
+        )}
+      </>
+      {screen < 1200 && (
+        <div className={s.contenedorBoton}>
+          <button onClick={() => {setShow(!show)}} className={s.boton}>Toque aqui</button>
+        </div>
+      )}
     </div>
   );
 }
